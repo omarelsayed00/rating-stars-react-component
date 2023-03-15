869a28582e0e1e365b349aaea2bc7a167c616fd8
Omar Elsayed <omarelsayed0015@gmail.com>
2023-03-15 15:38:01 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Rating from "./Rating";
+
+function App() {
+  return (
+    <div className="App">
+      <Rating rating={4} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Rating/styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+export const RatingForm = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 18px;
+  width: 200px;
+  background-color: #fcf6e4;
+  border-radius: 8px;
+  height: 40px;
+  padding: 4px 10px;
+  h1 {
+    font-size: 12px;
+    font-weight: 600;
+    color: black;
+  }
+
+  p {
+    padding-top: 4px;
+    font-size: 10px;
+    color: #a3905d;
+  }
+  div {
+    display: flex;
+    align-items: center;
+    //gap: 0px;
+  }
+`;
